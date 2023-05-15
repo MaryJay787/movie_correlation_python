ffd1573d1d779e8eaa2e48df3bfaecea8047afc8
MaryJay787 <josephmaryam.mj@gmail.com>
2023-05-15 11:05:45 -0400

    Added code to find and view the dataset

--- movie_correlation_exploration.py
+++ movie_correlation_exploration.py
@@ -3,7 +3,15 @@ import seaborn as sns
 #%%
 import pandas as pd
 import numpy as np
-
+# Libraries needed to view the dataframe
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 import matplotlib
+
+plt.style.use('ggplot')
+from matplotlib.pyplot import figure
+
+#%%matplotlib inline
+matplotlib.rcParams['figure.figsize'] = (12,8)
+
+pd.options.mode.chained_assignment = None
